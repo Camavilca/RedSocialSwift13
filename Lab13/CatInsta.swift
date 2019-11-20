dbb2b61f79c83521e6d1a7aefb91eaadd788bc68
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-11-19 22:48:52 -0600

    fix

---
+++ Lab13/CatInsta.swift
@@ -0,0 +1,34 @@
+import Foundation
+import FirebaseDatabase
+
+struct CatInsta {
+    
+    let key:String!
+    let url:String!
+    
+    let itemRef: DatabaseReference?
+    
+    init(url:String, key:String) {
+        self.key = key
+        self.url = url
+        self.itemRef = nil
+    
+    }
+    
+    init(snapshot:DataSnapshot) {
+        
+        key = snapshot.key
+        itemRef = snapshot.ref
+        
+        let snapshotValue = snapshot.value as? NSDictionary
+        
+        if let imageUrl = snapshotValue?["url"] as? String {
+            url = imageUrl
+        } else {
+            url = ""
+        }
+    }
+    
+}
+
+
---
+++ Lab13/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+   
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
+}
